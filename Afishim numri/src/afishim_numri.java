ceb755c6a64e81509acf0054a36770a17adb8847
disar_000 <disaracuraj@hotmail.com>
2020-06-01 17:57:05 +0100

    Afishim numri

---
+++ Afishim numri/src/afishim_numri.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class afishim_numri {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int a;
+		System.out.println("Jep nje numer te plote");
+		a=input.nextInt();
+
+	}
+
+}
